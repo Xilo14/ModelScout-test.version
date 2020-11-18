4ad7c13437736c1d042902dd51a1da2d6a295feb
Xilosof <pro100temi444@gmail.com>
2020-11-19 00:16:11 +0300

    Init sln

---
+++ DbModelScoutEf/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DbModelScoutEf
+{
+    public class Class1
+    {
+    }
+}
---
+++ TgBotConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TgBotConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
