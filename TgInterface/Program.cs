5950eb9816bd0e569a45b299898ba684d3a2f3c1
Xilosof <pro100temi444@gmail.com>
2020-11-22 09:29:45 +0300

    Added start bot with statemachine

--- TgInterface/Program.cs
+++ TgInterface/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using System;
 
-namespace TgInterface
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace TgInterface {
+    class Program {
+        static string _apiKey = "965933220:AAF89Gzw8wPZvOTFlrKCXZ_yIOZTy5OTCPI";
+        static void Main (string[] args) {
+            var bot = new TelegramBotBase.BotBase<Forms.StartForm> (_apiKey);
+
+            bot.StateMachine = new TelegramBotBase.States.JSONStateMachine (
+                AppContext.BaseDirectory + "config\\states.json");
+
+            bot.Start ();
+
+            Console.WriteLine ("Bot started");
+
+            Console.ReadLine ();
+            bot.Stop ();
         }
     }
 }
