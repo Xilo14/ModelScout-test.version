5faa886e97f1750ac555138a16c4452acb5ba890
Xilosof <pro100temi444@gmail.com>
2020-11-22 20:47:59 +0300

    SetUp DbContext

---
+++ ModelScoutAPI/ModelScoutDbContext.cs
@@ -0,0 +1,25 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using ModelScoutAPI.Models;
+
+namespace ModelScoutAPI {
+    public class ModelScoutDbContext : DbContext {
+        public DbSet<User> Users { get; set; }
+        public DbSet<VkAcc> VkAccs { get; set; }
+        public DbSet<VkClient> VkClients { get; set; }
+
+
+        public ModelScoutDbContext(DbContextOptions<ModelScoutDbContext> options) 
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating (ModelBuilder modelBuilder) {
+            modelBuilder
+                .Entity<VkClient> ()
+                .Property (e => e.ClientStatus)
+                .HasConversion<int> ();
+        }
+    }
+}
