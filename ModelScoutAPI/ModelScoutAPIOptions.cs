ed91cbfa5eae23ad9b2aca4e1983edecf26d1c81
Xilosof <pro100temi444@gmail.com>
2020-12-03 18:27:04 +0300

    Added const string which define section in .json consig. This string used for binding config

--- ModelScoutAPI/ModelScoutAPIOptions.cs
+++ ModelScoutAPI/ModelScoutAPIOptions.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace ModelScoutAPI {
     public class ModelScoutAPIOptions {
+        public const string ModelScout = "ModelScout";
+
         public String DbConnectionString { get; set; } = null;
         public String CptchApiKey { get; set; } = null;
         public String CptchSoftId { get; set; } = null;
