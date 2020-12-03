95037752784744773579a3e00f07f7dadfe03310
Xilosof <pro100temi444@gmail.com>
2020-12-03 18:30:56 +0300

    Added static class which create ModelScoutAPI`s and save him in the pool for further usage

---
+++ ModelScoutAPI/ModelScoutAPIPooler.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+
+namespace ModelScoutAPI
+{
+    public static class ModelScoutAPIPooler
+    {
+        public static ModelScoutAPIOptions DefaultOptions { get; set; } = new ModelScoutAPIOptions () {
+            CptchApiKey = "",
+            CptchSoftId = "",
+            DbConnectionString = "",
+        };
+        private static Dictionary<long, ModelScoutAPI> _pool = new Dictionary<long, ModelScoutAPI>();
+
+        public static async Task<ModelScoutAPI> GetOrCreateApi(long ChatId, ModelScoutAPIOptions Options)
+        {
+            ModelScoutAPI api;
+            
+            if (!_pool.TryGetValue(ChatId, out api))
+            {
+                api = new ModelScoutAPI(Options, (int)ChatId);
+                _pool.Add(ChatId, api);
+            }
+            return api;
+        }
+        public static async Task<ModelScoutAPI> GetOrCreateApi(long ChatId)
+            => await GetOrCreateApi(ChatId, ModelScoutAPIPooler.DefaultOptions);
+
+    }
+}
