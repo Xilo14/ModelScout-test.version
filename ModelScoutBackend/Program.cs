a66120880901f77b5f8cea3ea2485dca1cd001fc
Xilosof <pro100temi444@gmail.com>
2021-08-12 18:03:56 +0300

    feat: add logging of unhandled exceptions

--- ModelScoutBackend/Program.cs
+++ ModelScoutBackend/Program.cs
@@ -9,82 +9,101 @@ using Quartz.Impl;
 using Quartz.Impl.Matchers;
 using Quartz.Listener;
 using Serilog;
+using Serilog.Context;
 using Serilog.Formatting.Compact;
+
 
 namespace ModelScoutBackend {
     public class Program {
         public static ModelScoutAPIOptions MainOptions;
 
-        private static async Task Main (string[] args) {
+        private static async Task Main(string[] args) {
             // config Serilog logger 
-            Log.Logger = new LoggerConfiguration ()
-                .MinimumLevel.Debug ()
-                .Enrich.FromLogContext ()
-                .WriteTo.Console ()
-                .WriteTo.File (new CompactJsonFormatter (), "./logs/msBackend/json/log.json", rollingInterval : RollingInterval.Day)
-                .WriteTo.File ("./logs/msBackend/txt/log.txt", rollingInterval : RollingInterval.Day)
-                .CreateLogger ();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(new CompactJsonFormatter(), "./logs/msBackend/json/log.json", rollingInterval: RollingInterval.Day)
+                .WriteTo.File("./logs/msBackend/txt/log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
             // config and setup Microsoft.Logging for Quartz 
-            using var loggerFactory = LoggerFactory.Create (builder => {
+            using var loggerFactory = LoggerFactory.Create(builder => {
                 builder
-                    .ClearProviders ()
-                    .AddFilter ("Microsoft", Microsoft.Extensions.Logging.LogLevel.Warning)
-                    .AddFilter ("System", Microsoft.Extensions.Logging.LogLevel.Warning)
-                    .SetMinimumLevel (Microsoft.Extensions.Logging.LogLevel.Debug)
-                    .AddSerilog (dispose: true);
+                    .ClearProviders()
+                    .AddFilter("Microsoft", Microsoft.Extensions.Logging.LogLevel.Warning)
+                    .AddFilter("System", Microsoft.Extensions.Logging.LogLevel.Warning)
+                    .SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug)
+                    .AddSerilog(dispose: true);
             });
 
-            Quartz.Logging.LogContext.SetCurrentLogProvider (loggerFactory);
+            Quartz.Logging.LogContext.SetCurrentLogProvider(loggerFactory);
+            VkApisManager.logger = loggerFactory.CreateLogger<VkNet.VkApi>();
 
-            var builder = new ConfigurationBuilder ()
-                .SetBasePath (Directory.GetCurrentDirectory ())
-                .AddJsonFile ("appsettings.json");
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
 
-            var configuration = builder.Build ();
+            var configuration = builder.Build();
 
-            MainOptions = new ModelScoutAPIOptions ();
+            MainOptions = new ModelScoutAPIOptions();
 
-            configuration.GetSection (ModelScoutAPIOptions.ModelScout)
-                .Bind (MainOptions);
+            var currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(
+                (object sender, UnhandledExceptionEventArgs args) => {
+                    var e = (Exception)args.ExceptionObject;
+
+                    using (LogContext.PushProperty("exception", e)) {
+                        Log.Fatal("Unhandled exception!" +
+                            $"\nMessage: {e.Message}" +
+                            $"\nSource: {e.Source}" +
+                            $"\nStackTrace: {e.StackTrace}");
+                    }
+                });
+
+            configuration.GetSection(ModelScoutAPIOptions.ModelScout)
+                .Bind(MainOptions);
+
+            ModelScoutAPIPooler.DefaultOptions = MainOptions;
+            VkApisManager.modelScoutAPIOptions = MainOptions;
 
             // Grab the Scheduler instance from the Factory
-            StdSchedulerFactory factory = new StdSchedulerFactory ();
-            IScheduler scheduler = await factory.GetScheduler ();
+            var factory = new StdSchedulerFactory();
+            var scheduler = await factory.GetScheduler();
 
             // define the job and tie it to our HelloJob class
-            JobKey mainJobKey = new JobKey ("MainJob", "group1");
-            IJobDetail job = JobBuilder.Create<MainJob> ()
-                .WithIdentity (mainJobKey)
-                .Build ();
+            var mainJobKey = new JobKey("MainJob", "group1");
+            var job = JobBuilder.Create<MainJob>()
+                .WithIdentity(mainJobKey)
+                .Build();
 
             // Trigger the job to run now, and then repeat every 10 seconds
-            ITrigger trigger = TriggerBuilder.Create ()
-                .WithIdentity ("trigger1", "group1")
-                .StartNow ()
-                .Build ();
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .Build();
 
-            JobChainingJobListener chain = new JobChainingJobListener ("testChain");
-            chain.AddJobChainLink (mainJobKey, mainJobKey);
+            var chain = new JobChainingJobListener("testChain");
+            chain.AddJobChainLink(mainJobKey, mainJobKey);
 
             // Tell quartz to schedule the job using our trigger
-            await scheduler.ScheduleJob (job, trigger);
+            await scheduler.ScheduleJob(job, trigger);
 
-            scheduler.ListenerManager.AddJobListener (chain, GroupMatcher<JobKey>.AnyGroup ());
+            scheduler.ListenerManager.AddJobListener(chain, GroupMatcher<JobKey>.AnyGroup());
 
-            Console.WriteLine ("Press any key to start the schedular");
-            await Console.In.ReadLineAsync ();
+            Console.WriteLine("Press any key to start the schedular");
+            await Console.In.ReadLineAsync();
 
             // and start it off
-            await scheduler.Start ();
+            await scheduler.Start();
 
             // some sleep to show what's happening
-            Console.WriteLine ("Press any key to close the application");
-            await Console.In.ReadLineAsync ();
+            Console.WriteLine("Press any key to close the application");
+            await Console.In.ReadLineAsync();
 
             // and last shut down the scheduler when you are ready to close your program
-            await scheduler.Shutdown ();
-            Console.ReadKey ();
+            await scheduler.Shutdown();
+            Console.ReadKey();
 
         }
     }
